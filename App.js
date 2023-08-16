3dee7cfd9db55c4e2d8433e4cb031d1da4423d0e
Lakshan_IT19951836 <it19951836@my.sliit.lk>
2023-08-16 16:03:53 +0530

    styles changes

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TextHeading1 from './src/components/atoms/Texts/TextHeading1'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextHeading1 value={"Test"} fontSize={10}/>
       <StatusBar style="auto" />
     </View>
   );
--- src/components/atoms/Texts/TextHeading1.js
+++ src/components/atoms/Texts/TextHeading1.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+export default function TextHeading1({ value, fontSize }) {
+  
+    const textStyles = {
+    ...styles.Heading1,
+    fontSize: fontSize || styles.Heading1.fontSize, // Use prop value or default
+  };
 
-export default function TextHeading1({value}) {
-    return (
-        <Text style={styles.Heading1}>{value}</Text>
-    )
+  return <Text style={textStyles}>{value}</Text>;
 }
 
 const styles = StyleSheet.create({
-    Heading1: {
-        fontSize: 50,
-        fontWeight: 'bold',
-        marginBottom: 16,
-        textAlign: 'left',
-    },
-})
+  Heading1: {
+    fontSize: 50,
+    fontWeight: "bold",
+    marginBottom: 16,
+    textAlign: "left",
+  },
+});
