c7e7ca9b17dd5643927b324efaabb7103ca21508
MadhaviImashi <madhaviimashi@gmail.com>
2023-08-17 09:52:45 +0530

    text fields reused

--- src/components/molecules/ItemCards/CardTripsHistoryItem.js
+++ src/components/molecules/ItemCards/CardTripsHistoryItem.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { StyleSheet, View, ImageBackground, Text } from "react-native";
 import TransparentCard from "./../../../assets/images/TransparaentCard/TransparentCard.png";
+import TextHeading from "../../atoms/texts/TextHeading";
+import TextBody from "../../atoms/texts/TextBody";
 
 export default function CardTripsHistoryItem({ source, children }) {
   return (
 
-      <ImageBackground source={TransparentCard} style={styles.background}>
+    <>
+    {/* <ImageBackground source={TransparentCard} style={styles.background}>
         <View style={styles.content}>
-          <Text style={[styles.titleHead, { fontWeight: 700, fontSize: 14 }]}>
-            Trip #000648
-          </Text>
-          <Text style={[styles.description, { fontWeight: 500, fontSize: 12 }]}>
-            Distance 364.67M Km
-          </Text>
+          <Text style={[styles.titleHead, { fontWeight: 700, fontSize: 14 }]}>Trip #000648</Text>
+          <Text style={[styles.description, { fontWeight: 500, fontSize: 12 }]}>Distance 364.67M Km</Text>
         </View>
         <View style={styles.content}>
           <Text style={[styles.titleHead, { fontSize: 10 }]}>From Earth</Text>
@@ -26,7 +25,22 @@ export default function CardTripsHistoryItem({ source, children }) {
             Card Description
           </Text>
         </View>
-      </ImageBackground>
+        </ImageBackground> */}
+          <ImageBackground source={TransparentCard} style={styles.background}>
+          <View style={styles.content}>
+            <TextHeading value={'Trip #000648'} fontSize={14} lineHeight={20}></TextHeading>
+            <TextBody value={'Distance 364.67M Km'} fontSize={12} lineHeight={20} fontWeight={500}></TextBody>
+          </View>
+          <View style={styles.content}>
+            <TextBody value={'From Earth'} fontSize={10}></TextBody>
+            <TextBody value={'To Mars'} fontSize={10}></TextBody>
+          </View>
+          <View style={styles.content}>
+            <TextHeading value={'Card Title'} fontSize={14} lineHeight={20}></TextHeading>
+            <TextBody value={'Card Description'} fontSize={12} lineHeight={20} fontWeight={500}></TextBody>
+          </View>
+        </ImageBackground>
+    </>
     
   );
 }
@@ -44,14 +58,13 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingLeft: 16,
     paddingRight: 16,
-    
   },
-  titleHead: {
-    color: "white",
-    lineHeight:20
-  },
-  description: {
-    color: "white",
-    fontSize: 10,
-  },
+  // titleHead: {
+  //   color: "white",
+  //   lineHeight:20
+  // },
+  // description: {
+  //   color: "white",
+  //   fontSize: 10,
+  // },
 });
